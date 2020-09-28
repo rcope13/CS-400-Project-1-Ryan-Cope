c5726d68717b250c58981b9d9446b6fb64bcf960
rcope13 <71539008+rcope13@users.noreply.github.com>
2020-09-27 22:32:33 -0500

    Add files via upload
    fixed many errors

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,4 +1,3 @@
-import java.nio.file.NoSuchFileException;
 import java.util.Scanner;
 
 public class FrontEnd {
@@ -8,42 +7,40 @@ public class FrontEnd {
 		String command = null;
 		Scanner scnr = new Scanner(System.in);
 		String prompt = "What would you like to do? To see the commands, type c.";
-		HashTableMap<String,CourseReading> genericHashTable;
-		
-		//this is where you decide to create a new database or use the default
-		System.out.println("Now running Check Your Texts. Would you like create a new database or import the default database?"
-				+ " To create a new database, input n, and to use the default database, input d.");
+		HashTableMap<String, CourseReading> genericHashTable;
+
+		// this is where you decide to create a new database or use the default
+		System.out.println(
+				"Now running Check Your Texts. Would you like create a new database or import the default database?"
+						+ " To create a new database, input n, and to use the default database, input d.");
 		command = scnr.nextLine().toLowerCase().trim();
 		boolean pickedDatabase = false;
-		while(pickedDatabase == false) {
-			if((!command.equals("n")) && (!command.equals("d"))) {
-				System.out.println("Please input a valid command.");
-				command = scnr.nextLine().toLowerCase().trim();
-			}
-			if(command.equals("d")) {
+		while (pickedDatabase == false) {
+			if (command.equals("d")) {
 				genericHashTable = Wrangle.readCSV("src/textbook_file.csv");
-				if(genericHashTable != null) {
+				if (genericHashTable != null) {
 					pickedDatabase = true;
 				}
-				System.out.println("Something went wrong. Try another command.");
-				command = scnr.nextLine().toLowerCase().trim();
+				if (genericHashTable == null) {
+					System.out.println("Something went wrong. Try another command.");
+					command = scnr.nextLine().toLowerCase().trim();
+				}
 			}
-			if(command.equals("n")) {
+			if (command.equals("n")) {
 				genericHashTable = new HashTableMap<String, CourseReading>();
 				pickedDatabase = true;
 			}
+			if ((!command.equals("n")) && (!command.equals("d"))) {
+				System.out.println("Please input a valid command.");
+				command = scnr.nextLine().toLowerCase().trim();
+			}
 		}
-		
-		System.out.println("Your database has been initialized. What would you like to do now? For a list of commands, input c.");
+
+		System.out.println(
+				"Your database has been initialized. What would you like to do now? For a list of commands, input c.");
 		command = scnr.nextLine().toLowerCase().trim();
 
 		while (quit == false) {
-			// check for valid command
-			if ((!command.equals("c")) && (!command.equals("g")) && (!command.equals("p")) && (!command.equals("r")) && (!command.equals("q"))) {
-				System.out.println("Must use valid command. " + prompt);
-				command = scnr.nextLine().toLowerCase().trim();
-			}
-
 			// outputs information corresponding to the "c" command
 			if (command.equals("c")) {
 				System.out.println("List of Commands:");
@@ -59,7 +56,7 @@ public class FrontEnd {
 			if (command.equals("g")) {
 				System.out.println("You have chosen to get a course from an ISBN. Please input a valid ISBN.");
 				String inputISBN = scnr.nextLine().trim();
-				//run the getHashIndex(inputISBN) command
+				// run the getHashIndex(inputISBN) command
 			}
 
 			// outputs information corresponding to the "p" command
@@ -70,19 +67,28 @@ public class FrontEnd {
 				String inputTitle = scnr.nextLine().trim();
 				System.out.println("Please input a valid course name.");
 				String inputCourseName = scnr.nextLine().trim();
-				//run the insert(inputISBN, inputTitle, inputCourseName) command
+				// run the insert(inputISBN, inputTitle, inputCourseName) command
 			}
 
 			// outputs information corresponding to the "r" command
 			if (command.equals("r")) {
-				System.out.println("You have chosen to remove a course. Please input a valid ISBN to check for the course.");
+				System.out.println(
+						"You have chosen to remove a course. Please input a valid ISBN to check for the course.");
 				String inputISBN = scnr.nextLine().trim();
-				//run the remove(inputISBN) command
+				// run the remove(inputISBN) command
 			}
+
 			// outputs information corresponding to the "q" command
 			if (command.equals("q")) {
 				System.out.println("Quitting the program.");
 				quit = true;
+			}
+
+			// check for valid command
+			if ((!command.equals("c")) && (!command.equals("g")) && (!command.equals("p")) && (!command.equals("r"))
+					&& (!command.equals("q"))) {
+				System.out.println("Must use valid command. " + prompt);
+				command = scnr.nextLine().toLowerCase().trim();
 			}
 		}
 
